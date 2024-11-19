3e7eeff81400235442e31b4b13ad814740d77d9c
macrolive578 <macrolive578@gmail.com>
2024-11-19 16:16:31 -0500

    first

--- Config/db.php
+++
@@ -1,5 +0,0 @@
-<?php
-define("base_url","localhost/proyecto")
-define("controller_default","")
-define()
-?>
--- index.php
+++ index.php
@@ -1,5 +1,35 @@
 <?php
+    session_start();
+    require_once "autoload.php";
+    require_once "config/db.php";
+    require_once "config/parameters.pphp";
+
     require_once "views/layout/header.php";
     require_once "views/layout/sidebar.php";
-    require_once "views/producto/destacados.php";
+    //require_once "views/producto/destacados.php";
     require_once "views/layout/footer.php";
+    function show_error(){
+        $error = new errorController();
+        $error->index();
+    }
+    if(isset($_GET['controller'])){
+        $nombre_Controlador = $_GET['controller'].+'Controller';
+    }else(!isset($_GET['controller']) && !isset($_GET['action'])){
+        $nombre_Controlador = controller_default;
+    }else{
+        show_error();
+        exit();
+    }
+    if(class_exists($nombre_controlador)){
+        $controlador = new $nombre_Controlador();
+
+        if(isset($_GET['action']) && method_exists($controlador,$_GET['action'])){
+            $action = $_GET['action'];
+            $controlador->$action();
+        } elseif(!isset($_GET['controller']) && !isset($_GET['action'])){
+            $action_default = action_default;
+            $controlador->$action_default();
+        } else {
+            show_error();
+        }
+    }
